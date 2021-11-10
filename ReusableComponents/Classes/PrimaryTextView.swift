17b2467c8c1bd587962f48e99660c701442e4aab
Alexander Snitko <a.snitko@bsl.dev>
2021-11-10 18:48:07 +0300

    animations with placeholder

--- ReusableComponents/Classes/PrimaryTextView.swift
+++ ReusableComponents/Classes/PrimaryTextView.swift
@@ -7,51 +7,70 @@
 
 import UIKit
 
+enum TrailingButtonTypes {
+    case clear
+    case password
+}
+
 @IBDesignable
 class PrimaryTextView: UIView {
     
     var floatingLabel: UILabel = UILabel(frame: CGRect.zero)
     var textField: UITextField = UITextField(frame: CGRect.zero)
-//    var floatingLabel: UILabel
+    var trailingButton: UIButton = UIButton(frame: CGRect.zero)
+    var textFieldYConstraint: NSLayoutConstraint!
+    var labelYConstraint: NSLayoutConstraint!
     
-    
-    @IBInspectable
-    var superviewBackgoundColor: UIColor = UIColor.gray {
+    var trailingButtonType: TrailingButtonTypes? {
         didSet {
-            self.backgroundColor = superviewBackgoundColor
-            self.setNeedsDisplay()
+            configureTrailingImage()
         }
     }
+    
     @IBInspectable
-    var floatingLabelHeight: CGFloat = 17
+    var trailingButtonImage: UIImage = UIImage()
+    
+    @IBInspectable
+    var viewColor: UIColor = UIColor.gray {
+        didSet {
+            self.backgroundColor = viewColor
+        }
+    }
+    
+    @IBInspectable
+    var viewCornerRadius: CGFloat = 16 {
+        didSet {
+            self.layer.cornerRadius = viewCornerRadius
+        }
+    }
+    
+    @IBInspectable
+    var labelHeight: CGFloat = 17
+    
+    @IBInspectable
+    var labelColor: UIColor = UIColor.gray {
+        didSet {
+            self.floatingLabel.textColor = labelColor
+        }
+    }
+    
+    @IBInspectable
+    var initLabelFont: UIFont = UIFont.systemFont(ofSize: 22) {
+        didSet {
+            self.floatingLabel.font = self.initLabelFont
+        }
+    }
+    
+    @IBInspectable
+    var finishedLabelFont: UIFont?
+    
     @IBInspectable
     var textFieldHeight: CGFloat = 22
     
     @IBInspectable
-    var textFieldPlaceholder: String? {
+    var placeholder: String? {
         didSet {
-            self.textField.placeholder = textFieldPlaceholder
-        }
-    }
-    
-    
-    
-    
-    
-    @IBInspectable
-    var floatingLabelColor: UIColor = UIColor.black {
-        didSet {
-            self.floatingLabel.textColor = floatingLabelColor
-            self.setNeedsDisplay()
-        }
-    }
-    
-    @IBInspectable
-    var floatingLabelFont: UIFont = UIFont.systemFont(ofSize: 13) {
-        didSet {
-            self.floatingLabel.font = self.floatingLabelFont
-            self.textField.font = self.floatingLabelFont
-            self.setNeedsDisplay()
+            self.floatingLabel.text = placeholder
         }
     }
     
@@ -60,47 +79,130 @@ class PrimaryTextView: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.textFieldPlaceholder = (self.textFieldPlaceholder != nil) ? self.textFieldPlaceholder : textField.placeholder
-        self.textField.placeholder = self.textFieldPlaceholder
+
+        self.textField.translatesAutoresizingMaskIntoConstraints = false
+        self.textField.clipsToBounds = true
+        self.floatingLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.floatingLabel.clipsToBounds = true
+        self.trailingButton.translatesAutoresizingMaskIntoConstraints = false
+        self.trailingButton.clipsToBounds = true
         
-        self.floatingLabel = UILabel(frame: CGRect.zero)
-        self.textField = UITextField(frame: CGRect(x: 16, y: (self.frame.size.height - self.textFieldHeight) / 2, width: self.frame.size.width - 32, height: self.textFieldHeight))
         self.addSubview(textField)
+        self.addSubview(floatingLabel)
+        self.addSubview(trailingButton)
         
-        self.textField.addTarget(self, action: #selector(self.addFloatingLabel), for: .editingDidBegin)
+        self.textFieldYConstraint = self.textField.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        self.labelYConstraint = self.floatingLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        NSLayoutConstraint.activate([
+            self.textField.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16),
+            self.textField.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
+            self.textField.heightAnchor.constraint(equalToConstant: textFieldHeight),
+            textFieldYConstraint,
+            self.floatingLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16),
+            self.floatingLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
+            self.floatingLabel.heightAnchor.constraint(equalToConstant: labelHeight),
+            labelYConstraint
+        ])
+        
+        self.floatingLabel.textColor = self.labelColor
+        self.floatingLabel.font = self.initLabelFont
+        self.floatingLabel.text = placeholder
+        
+        self.floatingLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapField)))
+        self.floatingLabel.isUserInteractionEnabled = true
+
         self.textField.addTarget(self, action: #selector(self.removeFloatingLabel), for: .editingDidEnd)
+        
+        setNeedsDisplay()
     }
     
-    @objc private func addFloatingLabel() {
-//        if self.text == "" {
-//            self.floatingLabel.textColor = self.floatingLabelColor
-//            self.floatingLabel.font = self.floatingLabelFont
-//            self.floatingLabel.text = self.textFieldPlaceholder
-//            self.floatingLabel.layer.backgroundColor = UIColor.white.cgColor
-//            self.floatingLabel.translatesAutoresizingMaskIntoConstraints = false
-//            self.floatingLabel.clipsToBounds = true
-//            self.floatingLabel.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.floatingLabelHeight)
-//
-//            self.layer.borderColor = self.activeBorderColor.cgColor
-//            self.addSubview(floatingLabel)
-//
-//            self.floatingLabel.bottomAnchor.constraint(equalTo: self.topAnchor, constant: -10).isActive = true
-//
-//            self.placeholder = ""
-//        }
-//        self.setNeedsDisplay()
+    @objc private func didTapField() {
+        textField.becomeFirstResponder()
+        if textField.text == "" || textField.text == nil {
+            NSLayoutConstraint.deactivate([
+                self.textFieldYConstraint,
+                self.labelYConstraint
+            ])
+            
+            self.textFieldYConstraint = textField.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12)
+            
+            self.labelYConstraint = floatingLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 7)
+            NSLayoutConstraint.activate([
+                textFieldYConstraint,
+                labelYConstraint
+            ])
+        }
+        
+        floatingLabel.animate(font: UIFont.systemFont(ofSize: 13), duration: 0.3)
+        UIView.animate(withDuration: 0.3) {
+            self.initLabelFont = self.finishedLabelFont ?? UIFont.systemFont(ofSize: 13)
+            self.layoutIfNeeded()
+        }
     }
+    
     
     @objc private func removeFloatingLabel() {
-//        if self.text == "" {
-//            UIView.animate(withDuration: 0.13) {
-//                self.subviews.forEach( { $0.removeFromSuperview() } )
-//                self.setNeedsDisplay()
-//            }
-//            self.placeholder = self._placeholder
-//        }
-//        self.layer.borderColor = UIColor.black.cgColor
-//    }
+        if self.textField.text == "" || self.textField.text == nil {
+            
+            NSLayoutConstraint.deactivate([
+                self.textFieldYConstraint,
+                self.labelYConstraint
+            ])
+            
+            textFieldYConstraint = self.textField.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            labelYConstraint = self.floatingLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            
+            NSLayoutConstraint.activate([
+                textFieldYConstraint,
+                labelYConstraint
+            ])
+            
+            floatingLabel.animate(font: UIFont.systemFont(ofSize: 22), duration: 0.3, aligment: .leading)
+        }
+        
+        
+
+        
+        UIView.animate(withDuration: 0.3) {
+            self.layoutIfNeeded()
+        }
+    }
     
+    private func configureTrailingImage() {
+        NSLayoutConstraint.activate([
+            self.trailingButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.trailingButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 16),
+            self.trailingButton.heightAnchor.constraint(equalToConstant: 12),
+            self.trailingButton.widthAnchor.constraint(equalToConstant: 12)
+        ])
+        self.trailingButton.isHidden = true
+        layoutIfNeeded()
+    }
+}
+
+extension UILabel {
+    
+    enum LabelAligment {
+        case leading
+        case trailing
+        case none
+    }
+    
+    func animate(font: UIFont, duration: TimeInterval, aligment: LabelAligment? = nil) {
+        let oldFrame = frame
+        let labelScale = self.font.pointSize / font.pointSize
+        self.font = font
+        let oldTransform = transform
+        transform = transform.scaledBy(x: labelScale, y: labelScale)
+        let newOrigin = frame.origin
+        frame.origin = oldFrame.origin // only for left aligned text
+        // frame.origin = CGPoint(x: oldFrame.origin.x + oldFrame.width - frame.width, y: oldFrame.origin.y) // only for right aligned text
+        setNeedsUpdateConstraints()
+        
+        UIView.animate(withDuration: duration) {
+            self.frame.origin = newOrigin
+            self.transform = oldTransform
+            self.layoutIfNeeded()
+        }
     }
 }
